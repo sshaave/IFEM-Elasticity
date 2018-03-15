6e1814241c35f00b32567580a1f35b20ca472c95
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2018-03-15 10:47:36 +0100

    Added: Some metrics needed for stiffness matrix calculation

--- Linear/KirchhoffLoveShell.C
+++ Linear/KirchhoffLoveShell.C
@@ -16,6 +16,7 @@
 #include "FiniteElement.h"
 #include "Utilities.h"
 #include "ElmMats.h"
+#include "Vec3Oper.h"
 #include "IFEM.h"
 
 
@@ -40,9 +41,46 @@ bool KirchhoffLoveShell::evalInt (LocalIntegral& elmInt,
 {
   ElmMats& elMat = static_cast<ElmMats&>(elmInt);
 
-  // Extract the co-variant tangent vectors at this point
+  // Covariant basis vectors
   Vec3 g1 = fe.G.getColumn(1);
   Vec3 g2 = fe.G.getColumn(2);
+
+  // Basis vector g3
+  Vec3 g3(g1,g2);
+  double lg3 = g3.length();
+
+  // Covariant metric gab
+  double gab11 = g1*g1;
+  double gab12 = g1*g2;
+  double gab22 = g2*g2;
+
+  // Contravariant metric gab_con and base vectors g_con
+  double invdetgab =  1.0/(gab11*gab22-gab12*gab12);
+  double gab_con11 =  invdetgab*gab22;
+  double gab_con12 = -invdetgab*gab12;
+  double gab_con22 =  invdetgab*gab11;
+  Vec3 g1_con = g1*gab_con11 + g2*gab_con12;
+  Vec3 g2_con = g1*gab_con12 + g2*gab_con22;
+
+  // Local cartesian coordinates
+  Vec3 e1(g1);     e1.normalize();
+  Vec3 e2(g2_con); e2.normalize();
+
+  // Transformation matrix T from contravariant to local cartesian basis
+  Matrix T(3,3);
+  double eg11 = e1*g1_con;
+  double eg12 = e1*g2_con;
+  double eg21 = e2*g1_con;
+  double eg22 = e2*g2_con;
+  T(1,1) =      eg11*eg11;
+  T(1,2) =      eg12*eg12;
+  T(1,3) = 2.0* eg11*eg12;
+  T(2,1) =      eg21*eg21;
+  T(2,2) =      eg22*eg22;
+  T(2,3) = 2.0* eg21*eg22;
+  T(3,1) = 2.0* eg11*eg21;
+  T(3,2) = 2.0* eg12*eg22;
+  T(3,3) = 2.0*(eg11*eg22+eg12*eg21);
 
   if (eM) // Integrate the mass matrix
     ; // TODO: Include the mass-matrix terms in elmMat.A[eM-1]
